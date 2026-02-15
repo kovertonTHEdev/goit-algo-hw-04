b511fa78b8a7c6a80300dd6621a69b9b3c641d41
kovertonTHEdev <gizana99p@gmail.com>
2026-02-15 15:11:21 +0200

    commit2

--- hw01/calculation_func.py
+++ hw01/calculation_func.py
@@ -1,17 +1,17 @@
 def total_salary (salaries_file): 
      try:
           with open(salaries_file, "r", encoding="UTF-8") as file:
-               total = 0 # акумулятор
-               count = 0 # акумулятор
+               total = 0
+               count = 0 
                for line in file:
-                    cleaned_line = line.strip() # прибирає пробіли 
-                    parts = cleaned_line.split(",") # робить список імен + ЗП
+                    cleaned_line = line.strip() 
+                    parts = cleaned_line.split(",") 
                     if len(parts) == 2: 
                          salary_text = parts[1]
                          try: 
                               salary = int(salary_text)
-                              total += salary # накопичення сум зарплат
-                              count+=1 #накопичення акум
+                              total += salary 
+                              count+=1 
                          except ValueError:
                               continue
      except FileNotFoundError:
@@ -19,5 +19,5 @@ def total_salary (salaries_file):
      if count == 0:
           return (0, 0)
      if count > 0:
-          average = total / count # cереднє вираховуємо
+          average = total / count 
           return (total, average)
