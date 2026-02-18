6814b258072d0f3d27f69493223a25847e0034cf
Andrew Nedoshivkin <gizana99p@gmail.com>
2026-02-18 20:34:55 +0200

    commit13

--- hw03/hw_03.py
+++ hw03/hw_03.py
@@ -1,8 +1,21 @@
 import sys
 from pathlib import Path
+
 from colorama import Fore, init
 
 init(autoreset=True)
+
+if len(sys.argv) < 2:
+    print("python hw_03.py <доступ_до_директорії>")
+    sys.exit(1)
+second_el = sys.argv[1]
+second_el_pathobj = Path(second_el)
+if not second_el_pathobj.exists():
+    print("Це розташування не існує")
+    sys.exit(1)
+if not second_el_pathobj.is_dir():
+    print("Цієї директорії не існує")
+    sys.exit(1)
 
 
 def function(path: Path, depth: int = 0):
@@ -12,7 +25,6 @@ def function(path: Path, depth: int = 0):
     except PermissionError:
         print(f"{indent}У вас не має доступу до {path.name}")
         return
-
     for file in elements:
         if file.is_dir():
             print(Fore.BLUE + f"{indent}+ {file.name}/")
@@ -21,24 +33,4 @@ def function(path: Path, depth: int = 0):
             print(Fore.GREEN + f"{indent}- {file.name}")
 
 
-def main():
-    if len(sys.argv) < 2:
-        print("python hw_03.py <доступ_до_директорії>")
-        sys.exit(1)
-
-    second_el = sys.argv[1]
-    second_el_pathobj = Path(second_el)
-
-    if not second_el_pathobj.exists():
-        print("Це розташування не існує")
-        sys.exit(1)
-
-    if not second_el_pathobj.is_dir():
-        print("Цієї директорії не існує")
-        sys.exit(1)
-
-    function(second_el_pathobj, 0)
-
-
-if __name__ == "__main__":
-    main()
+function(second_el_pathobj, 0)
